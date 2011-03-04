b059ae08a0515f817762f7a75d58591452bdedc2
Kristin Branson <bransonk@janelia.hhmi.org>
2011-03-04 14:12:22 -0500

    First pass at ExamineBehaviorStats

--- FlyBowlExamineBehaviorVariables.m
+++ FlyBowlExamineBehaviorVariables.m
@@ -101,8 +101,8 @@ queries(end+1:end+2) = {'data_type','stats_perframe_*'};
 queries(end+1:end+2) = {'flag_aborted',0};
 queries(end+1:end+2) = {'automated_pf','P'};
 queries(end+1:end+2) = {'experiment_name','FlyBowl_*'};
-%data = SAGEGetBowlData(queries{:});
-load('datacache_beh.mat','data');
+data = SAGEGetBowlData(queries{:});
+%load('datacache_beh.mat','data');
 % sort by date
 date = {data.exp_datetime};
 [~,order] = sort(date);
