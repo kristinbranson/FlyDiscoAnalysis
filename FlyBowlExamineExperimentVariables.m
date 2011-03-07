afa72139a9bcb61d347e0cf6dec8daf32399545e
Kristin Branson <bransonk@janelia.hhmi.org>
2011-03-07 11:10:47 -0500

    .

--- FlyBowlExamineExperimentVariables.m
+++ FlyBowlExamineExperimentVariables.m
@@ -105,8 +105,8 @@ queries(end+1:end+2) = {'data_type',data_types};
 queries(end+1:end+2) = {'flag_aborted',0};
 queries(end+1:end+2) = {'automated_pf','P'};
 queries(end+1:end+2) = {'experiment_name','FlyBowl_*'};
-%data = SAGEGetBowlData(queries{:},'removemissingdata',true);
-load('datacache.mat','data');
+data = SAGEGetBowlData(queries{:},'removemissingdata',true);
+%load('datacache.mat','data');
 % sort by date
 date = {data.exp_datetime};
 [~,order] = sort(date);
