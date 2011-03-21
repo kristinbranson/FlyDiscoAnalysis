35ba1c52b05fbc487d944d7aca87b746ac9d76f5
Kristin Branson <bransonk@janelia.hhmi.org>
2011-03-21 10:54:57 -0400

    Added code to DebugFlyBowlClassifySex2.m to examine results. Messing with stats_perframefeatures.txt.

--- parseExpDir.m
+++ parseExpDir.m
@@ -1,6 +1,6 @@
 function [res,success] = parseExpDir(expdir)
 
-res = regexp(expdir,'(?<line>[^/]+)_(?<effector>[^_]*)_Rig(?<rig>[0-9]+)Plate(?<plate>[0-9]+)Bowl(?<bowl>[A-Z]+)_(?<notstarted>(notstarted_)?)(?<date>[^_/]+)/?$','names','once');
+res = regexp(expdir,'^(?<pathstr>(.*[/\\])*)(?<line>[^/^\\]+)_(?<effector>[^_]*)_Rig(?<rig>[0-9]+)Plate(?<plate>[0-9]+)Bowl(?<bowl>[A-Z]+)_(?<notstarted>(notstarted_)?)(?<date>[^_/]+)/?$','names','once');
 success = ~isempty(res);
 if success,
   res.notstarted = strcmp(res.notstarted,'notstarted_');
