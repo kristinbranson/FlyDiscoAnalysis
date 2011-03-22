652c7b6be547b7dfce56efc5570ed8b4f40ce96b
Kristin Branson <bransonk@janelia.hhmi.org>
2011-03-22 19:00:43 -0400

    debugging making ctrax movie.

--- DebugFlyBowlMakeCtraxResultsMovie.m
+++ DebugFlyBowlMakeCtraxResultsMovie.m
@@ -10,23 +10,17 @@ end
 
 %% data locations
 
-if ispc,
-  
+if ispc,  
   settingsdir = 'E:\Code\FlyBowlAnalysis\settings';
-  analysis_protocol = '20110202_pc';
-  dataloc_params = ReadParams(fullfile(settingsdir,analysis_protocol,'dataloc_params.txt'));
-  expdir = 'pBDPGAL4U_TrpA_Rig1Plate10BowlA_20110202T105734';
-  expdir_read = fullfile(dataloc_params.rootreaddir,expdir);
-  
+  rootdir = 'E:\Data\FlyBowl\bowl_data';
 else
   settingsdir = '/groups/branson/bransonlab/projects/olympiad/FlyBowlAnalysis/settings';
-  protocol = '';
-  analysis_protocol = '20110211';
-  expdir = '/groups/sciserv/flyolympiad/Olympiad_Screen/fly_bowl/bowl_data/GMR_14B02_AE_01_TrpA_Rig2Plate14BowlD_20110204T094327';
+  rootdir = '/groups/sciserv/flyolympiad/Olympiad_Screen/fly_bowl/bowl_data';  
 end
-% [readframe,nframes,fid,vidinfo] = get_readframe_fcn(fullfile(expdir_read,'movie.ufmf'));
-% fclose(fid);
+analysis_protocol = '20110222';
+expdir = fullfile(rootdir,'GMR_14B02_AE_01_TrpA_Rig2Plate14BowlD_20110204T094327');
 
 %% run
 
-FlyBowlMakeCtraxResultsMovie(expdir);
+FlyBowlMakeCtraxResultsMovie(expdir,...
+  'analysis_protocol',analysis_protocol,'settingsdir',settingsdir);
