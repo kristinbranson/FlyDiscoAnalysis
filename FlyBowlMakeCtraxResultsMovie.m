b7302db858fc5afe4bf617fdba77e731c9693540
Kristin Branson <bransonk@janelia.hhmi.org>
2011-03-23 10:29:29 -0400

    Filename, frame number text was not appearing in results movies when created in nodisplay mode. Changed so that this text is added as a subtitle during the mencoder step.

--- FlyBowlMakeCtraxResultsMovie.m
+++ FlyBowlMakeCtraxResultsMovie.m
@@ -24,7 +24,7 @@ avifilestr = fullfile(expdir,dataloc_params.ctraxresultsavifilestr);
 avifile = [avifilestr,sprintf('_%05dto%05d',[ctraxresultsmovie_params.firstframes;ctraxresultsmovie_params.endframes]),'_temp.avi'];
 xvidfile = [avifilestr,sprintf('_%05dto%05d',[ctraxresultsmovie_params.firstframes;ctraxresultsmovie_params.endframes]),'.avi'];
 
-%% 
+%% create movie
 
 [~,basename] = fileparts(expdir);
 
@@ -39,6 +39,7 @@ succeeded = ...
   'movietitle',basename,...
   'compression','none',...
   'useVideoWriter',false,...
+  'titletext',false,...
   'avifileTempDataFile',[avifile,'-temp']);
 
 if ishandle(1),
@@ -49,12 +50,30 @@ if ~succeeded,
   error('Failed to create raw avi %s',avifile);
 end
 
-%%
+%% create subtitle file
 
-cmd = sprintf('mencoder %s -o %s -ovc xvid -xvidencopts bitrate=3000000',avifile,xvidfile);
+subtitlefile = fullfile(expdir,'subtitles.srt');
+fid = fopen(subtitlefile,'w');
+dt = [0,ctraxresultsmovie_params.endframes - ctraxresultsmovie_params.firstframes + 1];
+ts = cumsum(dt);
+for i = 1:numel(t0s),
+  fprintf(fid,'%d\n',i);
+  fprintf(fid,'%s --> %s\n',...
+    datestr(ts(i)/ctraxresultsmovie_params.fps/(3600*24),'HH:MM:SS,FFF'),...
+    datestr((ts(i+1)-1)/ctraxresultsmovie_params.fps/(3600*24),'HH:MM:SS,FFF'));
+  fprintf(fid,'%s, fr %d-%d\n\n',basename,...
+    ctraxresultsmovie_params.firstframes(i),...
+    ctraxresultsmovie_params.endframes(i));
+end
+fclose(fid);
+
+%% compress
+
+cmd = sprintf('mencoder %s -o %s -ovc xvid -xvidencopts bitrate=3000000 -sub %s -subfont-text-scale 2',avifile,xvidfile,subtitlefile);
 status = system(cmd);
 if status == 0,
   delete(avifile);
+  delete(subtitlefile);
 else
   error('Failed to compress avi to %s',xvidfile);
 end
