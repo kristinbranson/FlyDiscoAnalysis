516ea59be3254977d416bade8e38dc01493d1154
Kristin Branson <bransonk@janelia.hhmi.org>
2011-04-14 13:34:25 -0400

    Finished writing code for dynamically selecting flies to show in ctrax results movie.

--- DebugFlyBowlMakeCtraxResultsMovie.m
+++ DebugFlyBowlMakeCtraxResultsMovie.m
@@ -1,6 +1,10 @@
 %% set up paths
 
-bransonk/tracking/code/JCtrax/misc;
+if ispc,
+  addpath E:\Code\JCtrax\misc;
+  addpath E:\Code\JCtrax\filehandling;
+else
+  addpath /groups/branson/home/bransonk/tracking/code/JCtrax/misc;
   addpath /groups/branson/home/bransonk/tracking/code/JCtrax/filehandling;
 end
 
@@ -8,7 +12,7 @@ end
 
 if ispc,
   settingsdir = 'E:\Code\FlyBowlAnalysis\settings';
-  rootdir = 'E:\Data\FlyBowl\bowl_data';
+  rootdir = 'E:\Data\FlyBowl\CtraxTest20110407';
 else
   settingsdir = '/groups/branson/bransonlab/projects/olympiad/FlyBowlAnalysis/settings';
   %rootdir = '/groups/sciserv/flyolympiad/Olympiad_Screen/fly_bowl/bowl_data';
@@ -19,8 +23,7 @@ end
 
 analysis_protocol = '20110407';
 params = {'settingsdir',settingsdir,...
-  'analysis_protocol',analysis_protocol,...
-  'visible','on'};
+  'analysis_protocol',analysis_protocol};
 
 %%
 
