96f8257e904f05880937f929065e6f00a5325363
Allen Lee <leea30@bransonlab-ws1.(none)>
2013-08-02 17:08:02 -0400

    Be more careful about cleaning up old .mat files in per-frame dir.
    Former-commit-id: 2482243ffabb26a964717328bfb608851fdf59c0

--- @Trx/CleanPerFrameData.m
+++ @Trx/CleanPerFrameData.m
@@ -4,14 +4,29 @@ if ~exist('ns','var'),
   ns = 1:obj.nexpdirs;
 end
 if ~exist('fns','var') || isempty(fns),
-  fns = setdiff(Trx.PerFrameFieldNames(),Trx.TrajectoryFieldNames());
+  fns = -1; % clean up all non-traj mats in per-frame dir  
+  %fns = setdiff(Trx.PerFrameFieldNames(),Trx.TrajectoryFieldNames());
 end
 if ~iscell(fns),
   fns = {fns};
 end
-for i = 1:numel(fns),
-  fn = fns{i};
-  for n = ns,
+for n = ns,
+  if any(cellfun(@(x)isequal(x,-1),fns))
+    perframedir = fullfile(obj.expdirs{n},obj.dataloc_params.perframedir);
+    perframefiles = dir(perframedir);
+    perframefiles = perframefiles(~[perframefiles.isdir]);
+    perframefiles = {perframefiles.name}';
+    tfTmp = cellfun(@(x)~isempty(regexp(x,'\.mat$','once')),perframefiles);
+    perframefiles = perframefiles(tfTmp);
+    perframefiles = cellfun(@(x)x(1:end-4),perframefiles,'UniformOutput',false);
+    fnsCurr = setdiff(perframefiles,Trx.TrajectoryFieldNames());
+  else
+    fnsCurr = fns;
+  end
+      
+  for i = 1:numel(fnsCurr),
+    fn = fnsCurr{i};
+
     filename = obj.GetPerFrameFile(fn,n);
     if exist(filename,'file') && ~obj.DEBUG,
       fprintf('Deleting per-frame data file %s\n',filename);
