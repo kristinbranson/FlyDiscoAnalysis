4e5ad581fffa7afc537918fa2f096e635e02c87a
Allen <allenleetc@gmail.com>
2015-06-09 16:46:56 -0400

    build FlyBubbleRegisterTrx
    Former-commit-id: 98987d23944898ead0d8dfe45749500376d056d7

--- repo_snapshot.sh
+++ repo_snapshot.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd . >/dev/null
 cd $DIR
 echo $DIR
-/groups/flyprojects/home/leea30/bin/git-graph.sh | head -n 40
+/groups/flyprojects/home/leea30/bin/git-graph.sh | head -n 10
 #git rev-list HEAD | head -n 1
 git status --porcelain
 popd >/dev/null
