357c7e0f4d6da4361261bb9dd78b990fd4f5741e
Edwards <edwardsa@janelia.hhmi.org>
2015-07-09 17:28:35 -0400

    added SAGEGetBubbleData and SAGEListBubbleExperiments
    Former-commit-id: 11986b8feb71b852ed05d309ce7af0182d867328

---
+++ SAGEListBubbleExperiments.m
@@ -0,0 +1,12 @@
+function [datamerge,experiment_ids,iswarning] = SAGEListBubbleExperiments(varargin)
+
+[dataset,removemissingdata,leftovers] = myparse_nocheck(varargin,...
+  'dataset','metadata','removemissingdata',false);
+
+[datamerge,experiment_ids,iswarning] = SAGEGetBubbleData(leftovers{:},...
+  'dataset',dataset,'removemissingdata',removemissingdata);
+
+% for backwards compatibility
+for i = 1:numel(datamerge),
+  datamerge(i).line = datamerge(i).line_name;
+end
