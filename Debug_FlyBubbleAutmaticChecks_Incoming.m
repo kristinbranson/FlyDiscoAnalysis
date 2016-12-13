7f7441589b4f83022a1fd9874a0caffdb51c5929
robiea <robiea@janelia.hhmi.org>
2016-12-13 11:06:01 -0500

    added alice debug scripts
    Former-commit-id: 73a4c04673b2db2edabeff12213e41bf42942bbf

---
+++ Debug_FlyBubbleAutmaticChecks_Incoming.m
@@ -0,0 +1,32 @@
+%Debug script for autoincoming checks 12/13/2016
+
+%%
+addpath /groups/branson/home/bransonk/tracking/code/JCtrax/filehandling;
+addpath /groups/branson/home/bransonk/tracking/code/JCtrax/misc;
+settingsdir = '/groups/branson/home/robiea/Code_versioned/FlyBubbleAnalysis/settings';
+%rootdir = '/groups/sciserv/flyolympiad/Olympiad_Screen/fly_bowl/bowl_data';
+rootdir = '/tier2/branson/FlyBubble_testing/temp/testing_registration';
+expdir = fullfile(rootdir,'grooming_GMR_60E02_AE_01_CsChr_RigA_20160519T164300');
+
+%% parameters
+
+analysis_protocol = 'current_bubble';
+% analysis_protocol = 'current_non_olympiad_rubin_grooming';
+params = {'settingsdir',settingsdir,...
+  'analysis_protocol',analysis_protocol,...
+  'settingsdir',settingsdir,...
+    'datalocparamsfilestr','dataloc_params.txt',...
+  'debug',false,...
+  'min_barcode_expdatestr','20110301T000000',...
+  'logfid','pipeline_log.txt'};
+
+
+
+
+%%
+expdir = '/groups/branson/home/robiea/Projects_data/temp/cx_GMR_OL0077B_CsChr_RigB_20150901T133146';
+
+%% run once
+FlyBubbleAutomaticChecks_Incoming(expdir,'settingsdir',settingsdir,...
+  'analysis_protocol',analysis_protocol,...
+  'settingsdir',settingsdir);
---
+++ Debug_FlyBubbleRegisterTrx.m
@@ -0,0 +1,22 @@
+%DebugFlyBubbleRegisterTrx.m 4/15/2016
+% 
+% /groups/flyprojects/home/leea30/git/fba.build/bubble/current/run_FlyBubbleRegisterTrx.sh /groups/branson/bransonlab/projects/olympiad/MCR/v717 /tier2/branson/fly_bubble/01_tracked/social_GMR_22D03_AE_01_dTrpA1_RigA_20160325T134540 
+% settingsdir = ' /groups/flyprojects/home/olympiad/bubble_bin/FlyBubbleAnalysis/settings/ analysis_protocol current_bubble'
+%%
+addpath /groups/branson/home/bransonk/tracking/code/JCtrax/filehandling;
+addpath /groups/branson/home/bransonk/tracking/code/JCtrax/misc;
+settingsdir = '/groups/branson/home/robiea/Code_versioned/FlyBubbleAnalysis/settings';
+%rootdir = '/groups/sciserv/flyolympiad/Olympiad_Screen/fly_bowl/bowl_data';
+rootdir = '/tier2/branson/FlyBubble_testing/temp/testing_registration';
+expdir = fullfile(rootdir,'grooming_GMR_60E02_AE_01_CsChr_RigA_20160519T164300');
+
+%% parameters
+
+% analysis_protocol = 'current_bubble';
+analysis_protocol = 'current_non_olympiad_rubin_grooming';
+params = {'settingsdir',settingsdir,...
+  'analysis_protocol',analysis_protocol};
+
+%% run once
+
+FlyBubbleRegisterTrx(expdir,params{:});
