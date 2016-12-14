f1cb75e0465c12cea7829c3c767f0da1620b4cf5
robiea <robiea@janelia.hhmi.org>
2016-12-14 14:01:31 -0500

    added debug for ClassifySex and ComputerPerFrameFeatures
    Former-commit-id: 9bbcac9e2be6cfd6f6366f76f4dee56195379203

---
+++ Debug_FlyBubbleComputePerFrameFeatures.m
@@ -0,0 +1,20 @@
+% Debug_FlyBubbleComputePerFrameFeatures 12/14/2016
+
+%%
+addpath /groups/branson/home/robiea/Code_versioned/Jdetect_github/Jdetect/filehandling/;
+addpath /groups/branson/home/robiea/Code_versioned/Jdetect_github/Jdetect/misc;
+settingsdir = '/groups/branson/home/robiea/Code_versioned/FlyBubbleAnalysis/settings';
+%rootdir = '/groups/sciserv/flyolympiad/Olympiad_Screen/fly_bowl/bowl_data';
+rootdir = '/tier2/branson/FlyBubble_testing/temp/testing_registration';
+% expdir = fullfile(rootdir,'grooming_GMR_60E02_AE_01_CsChr_RigA_20160519T164300');
+expdir = '/groups/branson/home/robiea/Projects_data/temp/cx_GMR_OL0077B_CsChr_RigB_20150901T133146';
+
+%% parameters
+
+analysis_protocol = 'current_bubble';
+% analysis_protocol = 'current_non_olympiad_rubin_grooming';
+params = {'settingsdir',settingsdir,...
+  'analysis_protocol',analysis_protocol};
+
+%%
+FlyBubbleComputePerFrameFeatures(expdir,params{:})
