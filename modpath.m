646099f408c40c62ab3fce8c629affcb06174af5
Adam L. Taylor <taylora@janelia.hhmi.org>
2020-11-30 14:17:37 -0500

    Added FlyTracker as submodule, updated modpath.m accordingly

--- modpath.m
+++ modpath.m
@@ -12,12 +12,7 @@ function modpath()
   addpath(fullfile(path_to_this_folder, 'JAABA', 'perframe')) ;
   
   % Run the FlyTracker modpath script
-  % We assume FlyTracker is in a sibling folder named "FlyTracker"
-  % We'll include it as a submodule once things get sorted with that.
-  % Currently you need to use my private FlyTracker repo, at
-  %   /groups/branson/bransonlab/adam/FlyTracker
-  % Commit d23c0827607b4c5190321b65e7977c978bc04909 
-  flytracker_folder_path = fullfile(path_to_parent_folder, 'FlyTracker') ;
+  flytracker_folder_path = fullfile(path_to_this_folder, 'FlyTracker') ;
   flytracker_modpath_script_path = fullfile(flytracker_folder_path, 'modpath.m') ;
   run(flytracker_modpath_script_path) ;  
 end
