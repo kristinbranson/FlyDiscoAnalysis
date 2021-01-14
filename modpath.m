f22d207cbffb43baf80a94e957682ae9dcfeee1c
Adam L. Taylor <taylora@janelia.hhmi.org>
2021-01-14 16:13:28 -0500

    Updated modpath.m to work better when called from another modpath.m

--- modpath.m
+++ modpath.m
@@ -3,7 +3,7 @@ function modpath()
   
   path_to_this_script = mfilename('fullpath') ;
   path_to_this_folder = fileparts(path_to_this_script) ;
-  %path_to_parent_folder = fileparts(path_to_this_folder) ;
+  
   addpath(fullfile(path_to_this_folder, 'JAABA', 'filehandling')) ;
   addpath(fullfile(path_to_this_folder, 'JAABA', 'misc')) ;
   addpath(fullfile(path_to_this_folder, 'simplewing')) ;
@@ -15,4 +15,8 @@ function modpath()
   flytracker_folder_path = fullfile(path_to_this_folder, 'FlyTracker') ;
   flytracker_modpath_script_path = fullfile(flytracker_folder_path, 'modpath.m') ;
   run(flytracker_modpath_script_path) ;  
+  
+  % Finally, add this folder itself
+  addpath(path_to_this_folder) ;  % do this so that we don't have to stay in this folder
+  % Also useful b/c this file may be called from elsewhere
 end
