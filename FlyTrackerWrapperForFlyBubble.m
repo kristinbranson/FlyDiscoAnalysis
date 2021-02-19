50e9d062be504a9d83f3611b305ea7557c0b628e
Adam L. Taylor <taylora@janelia.hhmi.org>
2021-02-18 19:26:49 -0500

    Clearly labelled FlyTracker parent calibration file vs 'child' calibration file

--- FlyTrackerWrapperForFlyBubble.m
+++ FlyTrackerWrapperForFlyBubble.m
@@ -1,25 +1,28 @@
 function FlyTrackerWrapperForFlyBubble(expdir, settingsdir, analysis_protocol, dataloc_params)
   
     % Determine paths to needed files
+    flytracker_parent_calibration_file_name = dataloc_params.flytrackerparentcalibrationstr ;
+    flytracker_parent_calibration_file_path = fullfile(settingsdir, analysis_protocol, flytracker_parent_calibration_file_name) ;
     flytracker_calibration_file_name = dataloc_params.flytrackercalibrationstr ;
     flytracker_calibration_file_path = fullfile(settingsdir, analysis_protocol, flytracker_calibration_file_name) ;
     video_file_name = dataloc_params.moviefilestr ;
     video_file_path = fullfile(expdir, video_file_name) ;
     
-    num_cores = maxNumCompThreads;
+    num_cores = maxNumCompThreads ;
     
-    options.num_cores   = num_cores;
-    options.num_chunks  = options.num_cores*2;
-    options.save_JAABA  = true;
-    options.save_xls    = false;
-    options.save_seg    = false;
-    options.f_parent_calib = flytracker_calibration_file_path ;
-    options.force_calib = true;
-    options.expdir_naming = true;
-    options.fr_sample = 200;
+    options.num_cores   = num_cores ;
+    options.num_chunks  = options.num_cores*2 ;
+    options.save_JAABA  = true ;
+    options.save_xls    = false ;
+    options.save_seg    = false ;
+    options.f_parent_calib = flytracker_parent_calibration_file_path ;
+    options.force_calib = true ;
+    options.expdir_naming = true ;
+    options.fr_sample = 200 ;
+    f_calib = flytracker_calibration_file_path ;
     
-    vinfo = video_open(video_file_path);
-    tracker([], options, [], vinfo);
+    vinfo = video_open(video_file_path) ;
+    tracker([], options, f_calib, vinfo) ;
   
 %   % Convert the output to ctrax form
 %   [~,video_base_name] = fileparts(video_file_name) ;
