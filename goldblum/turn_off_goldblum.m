c0364f1f47b353b12767d359df72db6b1db38fe5
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-04-24 16:48:09 -0400

    Added option to set time to run goldblum via cron

--- goldblum/turn_off_goldblum.m
+++ goldblum/turn_off_goldblum.m
@@ -1,4 +1,4 @@
-function turn_off()
+function turn_off_goldblum()
     command_line = 'crontab -l | grep --invert-match ''#GOLDBLUM'' | crontab' ;   % Remove line containing #GOLDBLUM                       
     system_with_error_handling(command_line) ;    
 end
--- goldblum/turn_on_goldblum.m
+++ goldblum/turn_on_goldblum.m
@@ -1,4 +1,26 @@
-function turn_on()
+function turn_on_goldblum(hr, min)
+    % Install the goldblum job in crontab.
+    % Can give optional hr, min args, which specify the time to run, in 24-hour
+    % clock format.  I.e. turn_on_goldblum(23,11) sets it to run once a day at 11:11
+    % PM.  Default time is 10:00 PM if no args are given.
+    
+    if ~exist('hr', 'var') || isempty(hr) ,
+        hr = 22 ;
+    else
+        hr = round(hr) ;
+        if hr<0 || hr>23 ,
+            error('hr much be an integer between 0 and 23, inclusive') ;
+        end
+    end
+    if ~exist('min', 'var') || isempty(min) ,
+        min = 0 ;
+    else
+        min = round(min) ;
+        if min<0 || min>23 ,
+            error('min much be an integer between 0 and 59, inclusive') ;
+        end
+    end
+    
     user_name = get_user_name() ;
     index_of_ell_in_lab = regexp(user_name, 'lab$', 'once') ;
     if isempty(index_of_ell_in_lab) ,
@@ -36,10 +58,16 @@ function turn_on()
     hash_goldblum = '#GOLDBLUM' ;
     escaped_hash_goldblum = escape_string_for_bash(hash_goldblum) ;
         
-    command_line = sprintf('{ crontab -l | grep --invert-match %s; echo "00 22 * * *     flock --nonblock %s --command %s   #GOLDBLUM"; } | crontab', ...
+    command_line = sprintf('{ crontab -l | grep --invert-match %s; echo "%02d %02d * * *     flock --nonblock %s --command %s   #GOLDBLUM"; } | crontab', ...
+                           min, ...
+                           hr, ...
                            escaped_hash_goldblum, ...
                            escaped_destination_folder_path, ...
-                           escaped_core_command_line)      %#ok<NOPRT> % Run at 10 PM every day                       
-                       
+                           escaped_core_command_line)      %#ok<NOPRT> % Run at whenever every day                       
+    
+    % Clear out any pre-existing #GOLDBLUM crontab lines
+    turn_off_goldblum()
+
+    % Execute the command to turn on goldblum
     system_with_error_handling(command_line) ;    
 end
