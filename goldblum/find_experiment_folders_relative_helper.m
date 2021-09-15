f5326a9eeb7fddbcc74dd140b3d063e77d791ac9
Adam L. Taylor <taylora@janelia.hhmi.org>
2021-09-15 15:04:19 -0400

    Made find_experiment_folders() call find_experiment_folders_relative().  Got rid of excluded files for expected-to-fail experiments in check_for_pipeline_output_files.m

--- goldblum/find_experiment_folders_relative_helper.m
+++ goldblum/find_experiment_folders_relative_helper.m
@@ -6,8 +6,16 @@ function result = find_experiment_folders_relative_helper(root_path, parent_rela
 
     % Separate source file, folder names    
     file_names = entries(~is_entry_a_folder) ;
-    folder_names = entries(is_entry_a_folder) ;    
-
+    raw_folder_names = entries(is_entry_a_folder) ;    
+    
+    % Exclude the to-process folder if in the root path
+    if isempty(parent_relative_path) ,
+        is_to_process_folder = strcmp(raw_folder_names, 'to-process') ;
+        folder_names = raw_folder_names(~is_to_process_folder) ;
+    else
+        folder_names = raw_folder_names ;
+    end
+    
     % If the parent_path is an experiment folder, we're done
     if is_experiment_folder_given_contents(file_names) ,
         result = {parent_relative_path} ;
