0d01d2069f5a68457b5f7bf4ac9e3bbdd3093a6e
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-09-17 17:44:11 -0400

    Got rid of FlyDiscoCaboose() function, now call FlyDiscoPipeline() with appropriate arguments instead.

--- goldblum/goldblum_FlyDiscoCaboose_wrapper.m
+++ goldblum/goldblum_FlyDiscoCaboose_wrapper.m
@@ -16,7 +16,7 @@ function goldblum_FlyDiscoCaboose_wrapper(experiment_folder_path, settings_folde
     dt = datetime('now') ;
     date_as_string = string(datetime(dt, 'Format', 'uuuu-MM-dd')) ;
     time_as_string = string(datetime(dt, 'Format', 'HH:mm:ss')) ;
-    header_string = sprintf('Running FlyDiscoCaboose() on %s at %s', date_as_string, time_as_string) ;
+    header_string = sprintf('Running FlyDiscoPipeline() with caboose-appropriate settings on %s at %s', date_as_string, time_as_string) ;
     asterisks_string = repmat('*', [1 length(header_string)]) ;
     fprintf('\n\n\n\n\n') ;
     fprintf('%s\n', asterisks_string) ;    
@@ -32,6 +32,22 @@ function goldblum_FlyDiscoCaboose_wrapper(experiment_folder_path, settings_folde
     % Combine the caller-supplied analysis parameters with the defaults       
     analysis_parameters = merge_structs(default_analysis_parameters, overriding_analysis_parameters) ;
     
+    % Now turn off everything *except* the auto-checks-complete
+    analysis_parameters.doautomaticchecksincoming = 'off' ;
+    analysis_parameters.doflytracking = 'off' ;
+    analysis_parameters.doregistration = 'off' ;
+    analysis_parameters.doledonoffdetection = 'off' ;
+    analysis_parameters.dosexclassification = 'off' ;
+    analysis_parameters.dotrackwings = 'off' ;
+    analysis_parameters.docomputeperframefeatures = 'off' ;
+    analysis_parameters.docomputehoghofperframefeatures = 'off' ;
+    analysis_parameters.dojaabadetect = 'off' ;
+    analysis_parameters.docomputeperframestats = 'off' ;
+    analysis_parameters.doplotperframestats = 'off' ;
+    analysis_parameters.domakectraxresultsmovie = 'off' ;
+    analysis_parameters.doextradiagnostics = 'off' ;
+    analysis_parameters.doanalysisprotocol = 'off' ;
+    
     % Call the function to do the real work
-    FlyDiscoCaboose(experiment_folder_path, analysis_parameters) ;
+    FlyDiscoPipeline(experiment_folder_path, analysis_parameters) ;
 end
