5dd8a207cc4a5281005e74559a6c3a0f02a85fe5
Adam L. Taylor <taylora@janelia.hhmi.org>
2021-09-17 17:09:16 -0400

    Changed back to FlyDiscoPipeline() erroring only when things go seriously off the rails.

--- CheckACIResultsFileContents.m
+++ CheckACIResultsFileContents.m
@@ -1,19 +1,19 @@
-function CheckACIResultsFileContents(expdir, dataloc_params, stage)
+function do_continue_pipeline = CheckACIResultsFileContents(expdir, dataloc_params, stage)  %#ok<INUSD>
     aci_results_as_text_file_name = dataloc_params.automaticchecksincomingresultsfilestr ;
     aci_results_as_text_file_path = fullfile(expdir, aci_results_as_text_file_name) ;
     aci_results = ReadParams(aci_results_as_text_file_path) ;
     if isfield(aci_results, 'automated_pf') ,
         if strcmp(aci_results.automated_pf, 'P')  || strcmp(aci_results.automated_pf, 'U') ,
-            % do nothing, all is well
+            do_continue_pipeline = true ;
         elseif strcmp(aci_results.automated_pf, 'F') ,
-            msgs = { sprintf('ACI results file %s has automated_pf==F', aci_results_as_text_file_name) } ;
-            flydisco_pipeline_error(stage, msgs) ;
+            fprintf('ACI results file %s has automated_pf==F\n', aci_results_as_text_file_name) ;
+            do_continue_pipeline = false ;
         else
-            msgs = { sprintf('ACI results file %s has automated_pf value that is not P nor U', aci_results_as_text_file_name) } ;
-            flydisco_pipeline_error(stage, msgs) ;
+            fprintf('ACI results file %s has automated_pf value that is not P nor U\n', aci_results_as_text_file_name) ;
+            do_continue_pipeline = false ;
         end
     else
-        msgs = { sprintf('ACI results file %s has no automated_pf field', aci_results_as_text_file_name) } ;
-        flydisco_pipeline_error(stage, msgs) ;
+        fprintf('ACI results file %s has no automated_pf field\n', aci_results_as_text_file_name) ;
+        do_continue_pipeline = false ;
     end
 end
