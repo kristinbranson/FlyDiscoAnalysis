167612d12724f41b2064f7ae2e23ec0fe4319c82
Adam L. Taylor <adamlyletaylor@gmail.com>
2021-09-18 23:29:56 -0400

    Changes to make remote_sync() and remote_verify() less verbose by default

--- goldblum/remote_sync.m
+++ goldblum/remote_sync.m
@@ -1,4 +1,9 @@
-function remote_sync(source_user, source_host, source_path, dest_path)
+function remote_sync(source_user, source_host, source_path, dest_path, be_verbose)
+    % Deal with arguments
+    if ~exist('be_verbose', 'var') || isempty(be_verbose) ,
+        be_verbose = false ;
+    end
+    
     % record the start time
     tic_id = tic() ;
 
@@ -13,23 +18,33 @@ function remote_sync(source_user, source_host, source_path, dest_path)
     end
         
     % print an informative message
-    fprintf('Copying contents of\n%s@%s:%s\ninto\n%s\n... ', source_user, source_host, source_path, dest_path) ;
+    if be_verbose ,
+        fprintf('Copying contents of\n%s@%s:%s\ninto\n%s\n... ', source_user, source_host, source_path, dest_path) ;
+    end
     
     % call helper
     % All those zeros are the numbers of different kinds of failures so far
-    spinner = spinner_object() ;
+    if be_verbose ,
+        spinner = spinner_object() ;
+    else
+        spinner = spinner_object('mute') ;
+    end
     [n_copied, n_failed, n_dir_failed, n_verified, n_dir_failed_to_list, time_spent_copying] = ...
         remote_sync_helper(source_user, source_host, source_path, dest_path, 0, 0, 0, 0, 0, 0.0, spinner) ;  %#ok<ASGLU>
     spinner.stop() ;
     
     % print the number of files copied
     if n_failed==0 && n_dir_failed==0 && n_dir_failed_to_list==0 ,
-        fprintf('Successfully copied contents of\n%s@%s:%s\ninto\n%s\n', source_user, source_host, source_path, dest_path) ;
+        if be_verbose ,
+            fprintf('Successfully copied contents of\n%s@%s:%s\ninto\n%s\n', source_user, source_host, source_path, dest_path) ;
+        end
 
         % print the elapsed time
         elapsed_time = toc(tic_id) ;
-        fprintf("Elapsed time: %0.1f seconds\n", elapsed_time) ;
-        fprintf("Time spent copying: %0.1f seconds\n", time_spent_copying) ;
+        if be_verbose ,
+            fprintf("Elapsed time: %0.1f seconds\n", elapsed_time) ;
+            fprintf("Time spent copying: %0.1f seconds\n", time_spent_copying) ;
+        end
     else
         % throw an error if there were any failures
         error('remote_sync:did_fail', ...
--- goldblum/remote_verify.m
+++ goldblum/remote_verify.m
@@ -1,24 +1,40 @@
-function remote_verify(source_user, source_host, source_path, dest_path)
+function remote_verify(source_user, source_host, source_path, dest_path, be_verbose)
+    % Deal with arguments
+    if ~exist('be_verbose', 'var') || isempty(be_verbose) ,
+        be_verbose = false ;
+    end
+    
     % record the start time
     tic_id = tic() ;
 
     % print an informative message
-    fprintf('Verifying that contents of\n%s@%s:%s\nare present in\n%s\n... ', source_user, source_host, source_path, dest_path) ;
-        
+    if be_verbose ,
+        fprintf('Verifying that contents of\n%s@%s:%s\nare present in\n%s\n... ', source_user, source_host, source_path, dest_path) ;
+    end
+    
     % call helper
     % All those zeros are the numbers of different kinds of things that have been verified so far
-    spinner = spinner_object() ;
+    if be_verbose ,
+        spinner = spinner_object() ;
+    else
+        spinner = spinner_object('mute') ;
+    end        
     [n_files_verified, n_dirs_verified, n_file_bytes_verified] = ...
-        remote_verify_helper(source_user, source_host, source_path, dest_path, 0, 0, 0, spinner) ;  %#ok<ASGLU>  % this will throw if there's a verification failure
+        remote_verify_helper(source_user, source_host, source_path, dest_path, 0, 0, 0, spinner) ;  %#ok<ASGLU>  
+            % this will throw if there's a verification failure
     spinner.stop() ;    
     
     % print the number of files etc verified
     %fprintf("%d files verified\n", n_files_verified) ;
     %fprintf("%d folders verified\n", n_dirs_verified) ;
     %fprintf("%d file bytes verified\n", n_file_bytes_verified) ;
-    fprintf("Success: All files and folders in source are present in destination.\n")
+    if be_verbose ,
+        fprintf("Success: All files and folders in source are present in destination.\n")
+    end
 
     % print the elapsed time
     elapsed_time = toc(tic_id) ;
-    fprintf("Elapsed time: %0.1f seconds\n", elapsed_time) ;
+    if be_verbose ,
+        fprintf("Elapsed time: %0.1f seconds\n", elapsed_time) ;
+    end
 end
