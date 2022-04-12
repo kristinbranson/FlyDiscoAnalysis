71d9959ea1d86b57bff5b11e71e8303d325e9c2d
Adam L. Taylor <adamlyletaylor@gmail.com>
2022-04-12 14:48:23 -0400

    Updated README.md, added help documentation to goldblum/goldblum_analyze_experiment_folders.m

--- goldblum/goldblum_analyze_experiment_folders.m
+++ goldblum/goldblum_analyze_experiment_folders.m
@@ -1,5 +1,48 @@
 function goldblum_analyze_experiment_folders(folder_path_from_experiment_index, settings_folder_path, cluster_billing_account_name, ...
                                              do_use_bqueue, do_actually_submit_jobs, analysis_parameters_as_name_value_list)
+    %goldblum_analyze_experiment_folders  Runs the FlyDisco pipeline on a set of
+    %                                     experiment folders.
+    %
+    %   goldblum_analyze_experiment_folders(experiment_folder_list) runs the
+    %   FlyDisco pipeline on the experiment folders given in the cell array of
+    %   strings experiment_folder_list.  This is the function used by Goldblum to
+    %   run experiments through the FlyDisco pipeline.  Note in particular that this
+    %   function first runs the pipeline on all the experiments with the
+    %   automaticcheckscomplete stage turned off, then runs them all with *only* the
+    %   automaticcheckscomplete stage turned on.  By default, each run of the
+    %   pipeline is submitted as a single LSF cluster job.  The function
+    %   FlyDiscoPipeline() is used to run the pipeline.  See the documentation for
+    %   that function for more details.
+    %
+    %   goldblum_analyze_experiment_folders(experiment_folder_list, settings_folder_path)
+    %   uses analysis-protocol folders drawn from settings_folder_path instead of
+    %   the default setting folder path.  Again, see the documentation of
+    %   FlyDiscoPipeline() for more details.
+    %
+    %   goldblum_analyze_experiment_folders(..., cluster_billing_account_name)
+    %   bills any jobs submitted to the LSF cluster to the account specified by 
+    %   the string cluster_billing_account_name.  Examples might be 'branson',
+    %   'rubin', and 'scicompsoft'.  
+    %   
+    %   goldblum_analyze_experiment_folders(..., do_use_bqueue), if do_use_bqueue is
+    %   true, uses the bqueue_type() framework for submitting jobs to the LSF
+    %   cluster.  If do_use_bqueue is false, jobs are run locally, and without first
+    %   submitting them to a bqueue.  This option is mostly useful for debugging.
+    %   If missing or empty, do_use_bqueue defaults to true.
+    %
+    %   goldblum_analyze_experiment_folders(..., do_actually_submit_jobs), if 
+    %   do_use_bqueue is true, determines whether jobs are actually submitted to the
+    %   LSF queue or are simply run locally.  Again, this is mostly useful for
+    %   debugging purposes.  If do_use_bqueue is false, this argument is ignored.  
+    %   If missing or empty, do_actually_submit_jobs defaults to true.
+    %
+    %   goldblum_analyze_experiment_folders(..., analysis_parameters_as_name_value_list)
+    %   allows the caller to specify additional (key, value) pairs that are passed to
+    %   FlyDiscoPipeline().  For instance, a caller might set this argument to 
+    %   {'docomputeperframestats', 'off'} to disable the computeperframestats stage
+    %   of FlyDiscoPipeline().  See the documentation of FlyDiscoPipeline() for more
+    %   details, including a complete list of the supported keys.  If missing or
+    %   empty, analysis_parameters_as_name_value_list defaults to cell(1,0).
 
     % Process arguments                                
     if ~exist('do_use_bqueue', 'var') || isempty(do_use_bqueue) ,
