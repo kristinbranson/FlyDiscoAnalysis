63ca1d8868854da0b25ae82c8ba82e6e9ec56b59
Adam L. Taylor <adamlyletaylor@gmail.com>
2022-04-19 15:36:06 -0400

    Fixed bug with clean folder scripts

--- clean_experiment_folders.m
+++ clean_experiment_folders.m
@@ -3,6 +3,10 @@ function clean_experiment_folders(root_folder_name, do_delete_tracking_files, do
     % If do_delete_tracking_files is true, delete the tracking files also.
     % If do_delete_tracking_files is missing or empty, default is to *not* delete
     % tracking files.
+    % If do_wet_run is true, actually do the deleting.  Otherwise, just print what
+    % files would have been deleted.
+    % If do_wet_run is missing or empty, default is to *not* do the deleting.
+    %
     % PLEASE DON'T USE THIS ON ANYTHING BUT TEST EXPERIMENT FOLDERS.
     % IT DELETES ANY FILE WHOSE NAME DOES NOT MATCH A WHITELIST.
     
