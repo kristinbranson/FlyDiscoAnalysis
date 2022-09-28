5feaf22763dfdaae1c68ed9be8ae8950c939ce82
Kristin Branson <bransonk@janelia.hhmi.org>
2022-09-28 16:55:58 -0400

    Revert "Ignore - just updates to script for tracking with APT."
    This reverts commit 3dd9f0ad82d4ef38e3f5470de0273db95b0a2d14.

--- ScriptAPTTrack20220921.m
+++ ScriptAPTTrack20220921.m
@@ -54,7 +54,7 @@ set(hfigsample,'Units','pixels','Position',[100,100,1144,1144]);
 hfigxy = figure(124);
 set(hfigxy,'Units','pixels','Position',[100,100,1800,700]);
 maxvel = 10;
-for expi = 21:nexps,
+for expi = 1:20,
 %   if isnan(jobid(expi)),
 %     continue;
 %   end
@@ -74,9 +74,7 @@ for expi = 21:nexps,
   else
     continue;
   end
-  if exist(testoutdir,'dir'),
-    continue;
-  else
+  if ~exist(testoutdir,'dir'),
     mkdir(testoutdir);
   end
   [readframe,~,fid] = get_readframe_fcn(movfile);
