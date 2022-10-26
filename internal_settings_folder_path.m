4f3e327669fee882e3838b61a5f389bfcf03c129
Adam L. Taylor <taylora@janelia.hhmi.org>
2022-10-26 19:17:58 -0400

    Consolidated changes from last commit, to use internal_settings_folder_path() in (hopefully) all relevant places.

--- internal_settings_folder_path.m
+++ internal_settings_folder_path.m
@@ -1,13 +1,11 @@
 function result = internal_settings_folder_path()
-    % Get the path to the settings folder path that is included in the source repo,
+    % Get the path to the settings folder path that is 'included' in the source repo,
     % based on the location of this script file.  Make sure the folder exists before
     % returning.
+    % Nowadays this is normally a link to an external settings folder, so it's not
+    % really internal.
     this_script_path = mfilename('fullpath') ;
     fda_path = fileparts(this_script_path) ;
     result = fullfile(fda_path, 'settings') ;
-    if exist(result, 'dir') ,
-        % all is well
-    else
-        error('Internal settings path %s does not exist', result) ;        
-    end
+    % This is often overidden in client code, so don't check if it exists
 end
