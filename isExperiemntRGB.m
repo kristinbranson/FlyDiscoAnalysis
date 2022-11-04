5c95ee0babaa98209753e1a281667f3144f45093
ceschretter <ceschretter@gmail.com>
2022-11-04 14:50:39 -0400

    updated debug and IsExperimntRGB scripts for FlyBubblefRGB experiments

--- isExperiemntRGB.m
+++ isExperiemntRGB.m
@@ -1,5 +1,5 @@
 function result = isExperiemntRGB(metadata)
 
 result = ( strcmp(metadata.assay,'FlyBubbleRGB') || strcmp(metadata.assay,'FlyBowlRGB') || ...
-    strcmp(metadata.assay,'FlyBowlFRGB') || strcmp(metadata.assay,'FlyBubbleFRGB')  ) ;
+    strcmp(metadata.assay,'FlyBowlFRGB') || strcmp(metadata.assay,'FlyBubblefRGB')  ) ;
 end
