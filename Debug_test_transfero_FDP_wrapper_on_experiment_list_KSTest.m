7774cb000ea24d547e45fdefbf4e7153a4927c67
ceschretter <ceschretter@gmail.com>
2023-03-08 09:18:40 -0500

    updated debug transfero for testing APT and new settings dir

--- Debug_test_transfero_FDP_wrapper_on_experiment_list_KSTest.m
+++ Debug_test_transfero_FDP_wrapper_on_experiment_list_KSTest.m
@@ -11,7 +11,7 @@ do_actually_submit_jobs = false ;
 ssh_host_name = 'submit.int.janelia.org' ;
 
 
-settings_folder_path = '/groups/rubin/home/rubinlabuser/RubinFlyDiscoSettings/RubinFlyDiscoSettings/settings';
+settings_folder_path = '/groups/rubin/home/schretterc/Documents/FlyDiscoAnalysis/FlyDiscoAnalysis_1022/FlyDiscoAnalysis/settings/RubinFlyDiscoSettings/settings';
 analysis_protocol = '20230302_BubblefRGB_GtAg';
 
 optional_argument_list = ...
@@ -21,7 +21,8 @@ optional_argument_list = ...
 
 %% experiments 
 
-folder_path_from_experiment_index = {'/groups/rubin/home/schretterc/Documents/FlyBubblefRGB_Chrim/ForTestingAPT/20230222T161958_rig1_flyBubble1_SS37072_20XUASCsChrimsonattp18_20221129_AggAVLPChrim'};
+folder_path_from_experiment_index = {'/groups/rubin/home/schretterc/Documents/FlyBubblefRGB_Chrim/TestingNewCalibration_030623/20230306T084336_rig1_flyBubble1_SS36564_20XUASCsChrimsonattp18_20221128_AggaIPgChrim',...
+'/groups/rubin/home/schretterc/Documents/FlyBubblefRGB_Chrim/TestingNewCalibration_030623/20230222T163912_rig2_flyBubble1_SS37071_20XUASCsChrimsonattp18_20221129_AggAVLPChrim'};
 
 %% Call the testing function to do the real work
 test_transfero_FlyDiscoPipeline_wrapper_on_experiment_list(folder_path_from_experiment_index, ...
