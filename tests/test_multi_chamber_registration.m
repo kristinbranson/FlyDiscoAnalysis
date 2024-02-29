30f6928b262eff16c1638e29aa045a894bdf170d
Adam L. Taylor <taylora@janelia.hhmi.org>
2024-02-29 13:58:00 -0500

    multibubble-registration: Added test for registerForMultipleChambers.m

---
+++ tests/test_multi_chamber_registration.m
@@ -0,0 +1,60 @@
+rng(0);  % Want 'frozen' random numbers
+
+% Make sure get same numbers for a single chamber
+sample_count = 100 ;
+x = normrnd(0,1,[sample_count 1]) ;
+y = normrnd(0,1,[sample_count 1]) ;
+offX = normrnd(0,1) ;
+offY = normrnd(0,1) ;
+scale = exprnd(1) ;
+offTheta = unifrnd(-pi,+pi) ;
+[x_reg_single,y_reg_single] = registerForSingleChamber(x,y,offX,offY,offTheta,scale) ;
+[x_reg_multi,y_reg_multi] = registerForMultipleChambers(x,y,offX,offY,offTheta,scale) ;
+assert(all(abs(x_reg_single-x_reg_multi)<eps)) ;
+assert(all(abs(y_reg_single-y_reg_multi)<eps)) ;
+
+% Make sure it gets the chamber assignments right
+chamber_sample_count = 10 ;  % samples per chamber
+chamber_count = 9 ;
+sample_count = chamber_count * chamber_sample_count ;
+r_center_from_chamber_index = 3*[ 1 1 ; 1 2 ; 1 3 ; 2 1 ; 2 2 ; 2 3 ; 3 1 ; 3 2 ; 3 3 ]' ;  % 2 x chamber_count
+r_center_from_chamber_index = r_center_from_chamber_index(:,1:chamber_count) ;
+radius_full = unifrnd(0,1,[1 chamber_count chamber_sample_count]) ;
+theta_full = unifrnd(-pi,+pi,[1 chamber_count chamber_sample_count]) ;
+dx_full = radius_full .* cos(theta_full) ;
+dy_full = radius_full .* sin(theta_full) ;
+dr_full = vertcat(dx_full,dy_full) ;  % 2 x chamber_count x chamber_sample_count
+r_full = r_center_from_chamber_index + dr_full ;  % broadcast, 2 x chamber_count x chamber_sample_count
+chamber_index_from_chamber_index_and_chamber_sample_index = repmat(1:chamber_count, [1 1 chamber_sample_count]) ;
+r = reshape(r_full, [2 sample_count]) ;
+chamber_index_from_sample_index = reshape(chamber_index_from_chamber_index_and_chamber_sample_index, [1 sample_count]) ;
+% Shuffle the points
+sample_index_from_shuffled_sample_index = randperm(sample_count) ;
+r_shuffled = r(:,sample_index_from_shuffled_sample_index) ;
+chamber_index_from_shuffled_sample_index = chamber_index_from_sample_index(sample_index_from_shuffled_sample_index) ;
+scale = exprnd(1) ;
+offTheta = unifrnd(-pi,+pi) ;
+x_shuffled = r_shuffled(1,:) ;
+y_shuffled = r_shuffled(2,:) ;
+offX = -r_center_from_chamber_index(1,:) ;
+offY = -r_center_from_chamber_index(2,:) ;
+[x_reg_multi,y_reg_multi,output_chamber_index_from_shuffled_sample_index] = registerForMultipleChambers(x_shuffled,y_shuffled,offX,offY,offTheta,scale) ;
+assert(isequaln(output_chamber_index_from_shuffled_sample_index,chamber_index_from_shuffled_sample_index)) ;
+
+% Do single-chamber registration for each chamber's samples, compare with
+% multi-chamber registration results.
+x_reg_single = zeros(1,sample_count) ;
+y_reg_single = zeros(1,sample_count) ;
+for chamber_index = 1 : chamber_count ,
+  r_center = r_center_from_chamber_index(:,chamber_index) ;
+  offX = -r_center(1) ;
+  offY = -r_center(2) ;
+  is_in_chamber_from_shuffled_sample_index = (chamber_index_from_shuffled_sample_index == chamber_index) ;
+  x_shuffled_chamber = x_shuffled(is_in_chamber_from_shuffled_sample_index) ;
+  y_shuffled_chamber = y_shuffled(is_in_chamber_from_shuffled_sample_index) ;
+  [x_shuffled_chamber_reg,y_shuffled_chamber_reg] = registerForSingleChamber(x_shuffled_chamber,y_shuffled_chamber,offX,offY,offTheta,scale) ;
+  x_reg_single(is_in_chamber_from_shuffled_sample_index) = x_shuffled_chamber_reg ;
+  y_reg_single(is_in_chamber_from_shuffled_sample_index) = y_shuffled_chamber_reg ;  
+end
+assert(all(abs(x_reg_single-x_reg_multi)<eps)) ;
+assert(all(abs(y_reg_single-y_reg_multi)<eps)) ;
--- utility/registerForMultipleChambers.m
+++ utility/registerForMultipleChambers.m
@@ -1,4 +1,4 @@
-function [x_reg,y_reg] = registerForMultipleChambers(x,y,offX,offY,offTheta,scale)
+function [x_reg,y_reg,closest_chamber_index] = registerForMultipleChambers(x,y,offX,offY,offTheta,scale)
 % [x_reg,y_reg] = registerForMultipleChambers(x, y, offX, offY, offTheta, scale) 
 % 
 % Given unregistered coordinates <x,y>, add the offset <offX,offY>, translate
@@ -8,6 +8,7 @@ function [x_reg,y_reg] = registerForMultipleChambers(x,y,offX,offY,offTheta,scal
 % arrays, with the offset for the jth chamber in the jth row.  Note this
 % function does not support rotation.
 
+% Throw a more-scrutable error if x and y differ in shape
 sz = size(x);
 if numel(sz) ~= numel(size(y)) || ~all(sz == size(y)),
   error('Size of x and y must match');
@@ -24,14 +25,18 @@ else
   y_row = y(:)' ;
 end
 
+% Coerce offX and offY to col vectors
+offX_col = offX(:) ;
+offY_col = offY(:) ;
+
 % For each <x,y>, determine which chamber it belongs to
-distance_to_center = hypot(x_row+offX, y_row+offY) ;  % chamber_count x n
-[~, closest_chamber_index] = min(distance_to_center, [], 1) ;
+distance_to_center = hypot(x_row+offX_col, y_row+offY_col) ;  % chamber_count x n
+[~, closest_chamber_index_as_row] = min(distance_to_center, [], 1) ;
 
 % Get the center of the nearest chamber to each point
-n = numel(closest_chamber_index) ;
-offXFull = reshape(offX(closest_chamber_index), [1 n]) ;  % 1 x n, x-offset of nearest chamber center
-offYFull = reshape(offY(closest_chamber_index), [1 n]) ;  % 1 x n, y-offset of nearest chamber center
+n = numel(closest_chamber_index_as_row) ;
+offXFull = reshape(offX_col(closest_chamber_index_as_row), [1 n]) ;  % 1 x n, x-offset of nearest chamber center
+offYFull = reshape(offY_col(closest_chamber_index_as_row), [1 n]) ;  % 1 x n, y-offset of nearest chamber center
 
 % Compute the rotation matrix
 % Note that rotation is, in principle, about the mean of all the per-chamber origins, but this still
@@ -40,8 +45,8 @@ costheta = cos(offTheta); sintheta = sin(offTheta);
 R = [costheta,-sintheta;sintheta,costheta] ;
 
 % Do the transform
-Xt = [ x+offXFull ; ...
-       y+offYFull ] ;
+Xt = [ x_row+offXFull ; ...
+       y_row+offYFull ] ;
 Y = R * Xt * scale ;
 x_reg_row = Y(1,:) ;
 y_reg_row = Y(2,:) ;
@@ -51,7 +56,9 @@ if were_x_and_y_both_rows ,
   % do nothing
   x_reg = x_reg_row ;
   y_reg = y_reg_row ;
+  closest_chamber_index = closest_chamber_index_as_row ;
 else
   x_reg = reshape(x_reg_row, sz) ;
-  y_reg = rehapse(y_reg_row, sz) ;
+  y_reg = reshape(y_reg_row, sz) ;
+  closest_chamber_index = reshape(closest_chamber_index_as_row, sz) ;
 end
