32b52597419a53f4f0f9dd37975a03d8f4965eaf
Adam L. Taylor <taylora@janelia.hhmi.org>
2024-06-05 11:06:55 -0400

    deploy: Fixed small issue in does_protocol_pulse_count_equal_measured.m

--- utility/does_protocol_pulse_count_equal_measured.m
+++ utility/does_protocol_pulse_count_equal_measured.m
@@ -7,8 +7,8 @@ function [do_stim_counts_match, message] = does_protocol_pulse_count_equal_measu
 [indicatordata, dothrowerror] = myparse(varargin, 'indicatordata', [], 'dothrowerror', false) ;
 
 % Get locations of parameter files
-datalocparamsfile = fullfile(settingsdir,analysis_protocol,'dataloc_params.txt');
-dataloc_params = ReadParams(datalocparamsfile);
+datalocparamsfile = fullfile(settingsdir,analysis_protocol, 'dataloc_params.txt') ;
+dataloc_params = ReadParams(datalocparamsfile) ;
 
 % Don't need this check---this is called from a location that is only reached
 % if it's an optogenetic experiment.
@@ -28,43 +28,56 @@ dataloc_params = ReadParams(datalocparamsfile);
 %   return
 % end
 
-% read in protocol file if it exists
+% Check that LED protocol file exists
 ledprotocolfile = fullfile(expdir,dataloc_params.ledprotocolfilestr);
-indicatordatafile = fullfile(expdir,dataloc_params.indicatordatafilestr);
-
-if exist(ledprotocolfile,'file') && exist(indicatordatafile,'file')
-    raw_protocol = loadAnonymous(ledprotocolfile) ;
-    protocol = downmixProtocolIfNeeded(raw_protocol) ;
-
-
-    % If indictor not passed in, load detected stim data
-    if isempty(indicatordata)
-        indicatordatafile = fullfile(expdir,dataloc_params.indicatordatafilestr);
-        load(indicatordatafile,'indicatorLED');
-    else
-        indicatorLED = indicatordata.indicatorLED;
-    end
-
-    % Perform the test, error if it fails
-    nprotocolstim = distinct_pulse_count_from_single_channel_protocol(protocol) ;
-    ndetectedstim = numel(indicatorLED.startframe) ;
-    do_stim_counts_match = ( nprotocolstim == ndetectedstim ) ;
-    if do_stim_counts_match ,
-        message = sprintf('The number of detected stimuli (%g) is equal to the expected number of stimuli (%g) from the protocol file', ...
-            ndetectedstim, ...
-            nprotocolstim) ;
-    else
-        message = sprintf('The number of detected stimuli (%g) does not equal the expected number of stimuli (%g) from the protocol file', ...
-            ndetectedstim, ...
-            nprotocolstim) ;
-        if dothrowerror ,
-            error(message) ;  %#ok<SPERR>
-        end
-    end
-else
+if ~exist(ledprotocolfile,'file') ,
     do_stim_counts_match = false;
-    message = sprintf('Missing LED files: could not check stimuli number');
+    message = sprintf('Could not check number of stimuli: Missing LED protocol file %s', ledprotocolfile) ;
     if dothrowerror ,
         error(message) ;  %#ok<SPERR>
+    else
+        return
+    end
+end
+
+% Load the LED protocol file, downmix it to a single-channel protocol
+raw_protocol = loadAnonymous(ledprotocolfile) ;
+protocol = downmixProtocolIfNeeded(raw_protocol) ;
+
+% If indicator not passed in, load indicator data (as gleaned from the movie)
+if isempty(indicatordata)
+    % Make sure the indicator data file exists
+    indicatordatafile = fullfile(expdir,dataloc_params.indicatordatafilestr) ;
+    if ~exist(indicatordatafile,'file') ,
+        do_stim_counts_match = false;
+        message = sprintf('Could not check number of stimuli: Missing LED indicator data file %s', indicatordatafile) ;
+        if dothrowerror ,
+            error(message) ;  %#ok<SPERR>
+        else
+            return
+        end
+    end
+    % Load the indicator data file
+    load(indicatordatafile,'indicatorLED');
+else
+    indicatorLED = indicatordata.indicatorLED;
+end
+
+% Perform the test, error if it fails
+nprotocolstim = distinct_pulse_count_from_single_channel_protocol(protocol) ;
+ndetectedstim = numel(indicatorLED.startframe) ;
+do_stim_counts_match = ( nprotocolstim == ndetectedstim ) ;
+if do_stim_counts_match ,
+    message = sprintf('The number of detected stimuli (%g) is equal to the expected number of stimuli (%g) from the protocol file', ...
+                      ndetectedstim, ...
+                      nprotocolstim) ;
+else
+    message = sprintf('The number of detected stimuli (%g) does not equal the expected number of stimuli (%g) from the protocol file', ...
+                      ndetectedstim, ...
+                      nprotocolstim) ;
+    if dothrowerror ,
+        error(message) ;  %#ok<SPERR>
+    else
+        return
     end
 end
