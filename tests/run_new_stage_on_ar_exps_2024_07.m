9e63d435dead5b0c5a28e068e9cb979e07532454
Adam L. Taylor <taylora@janelia.hhmi.org>
2024-07-16 10:33:13 -0400

    main: Small change to tests/run_new_stage_on_ar_exps_2024_07.m

--- tests/run_new_stage_on_ar_exps_2024_07.m
+++ tests/run_new_stage_on_ar_exps_2024_07.m
@@ -42,7 +42,7 @@ optional_argument_list = horzcat(initial_optional_argument_list, analysis_parame
 % Find the experiments
 %s1 = loadAnonymous('/groups/branson/home/bransonlab/expdirs_VNC_20240711_VNC_passAandM_metadata_explist.mat') ;
 %folder_path_from_experiment_index_1 = s1' ;
-s1 = cell(0,1) ;
+folder_path_from_experiment_index_1 = cell(0,1) ;
 s2 = loadAnonymous('/groups/branson/home/bransonlab/expdirs_VNC_20240711_VNC2_passAandM_metadata_explist.mat') ;
 folder_path_from_experiment_index_2 = s2' ;
 folder_path_from_raw_experiment_index = vertcat(folder_path_from_experiment_index_1, folder_path_from_experiment_index_2) ;
