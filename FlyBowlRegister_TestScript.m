0487054feb3af24c95deaa491ef62dc30b4c5cbc
Adam L. Taylor <taylora@janelia.hhmi.org>
2024-07-25 21:36:04 -0400

    more-pflies: Purged likely-unused code

--- FlyBowlRegister_TestScript.m
+++
@@ -1,7 +0,0 @@
-% FlyBowlRegister_TestScript
-
-expdir = 'E:\Data\FlyBowl\Test1\GMR_12E07_AE_01_TrpA_Rig1Plate01BowlA_20101013T095913';
-protocol = '0.1';
-protocolFile = 'FlyBowlProtocol.txt';
-
-trx = FlyBowlRegister(expdir,protocol,'protocolFile',protocolFile);
