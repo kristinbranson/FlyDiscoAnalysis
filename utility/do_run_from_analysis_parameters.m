b7536fd901558a234745285bb7f47332f0030ede
Adam L. Taylor <taylora@janelia.hhmi.org>
2025-10-03 14:08:13 -0400

    deploy: Fixed test failure

--- utility/do_run_from_analysis_parameters.m
+++ utility/do_run_from_analysis_parameters.m
@@ -10,7 +10,13 @@ result = struct() ;
 for stage_index = 1 : stage_count ,
   stage_name = stage_names{stage_index} ;
   protocol_field_name = sprintf('do%s', stage_name) ;
-  raw_value = protocol.(protocol_field_name) ;
+  if isfield(protocol, protocol_field_name)
+    raw_value = protocol.(protocol_field_name) ;
+  else
+    % Likely the protocol predates the introduction of this stage, so just leave
+    % it off.
+    raw_value = 'off' ;
+  end
   result.(stage_name) = coerce_to_on_off_force(raw_value) ;
 end
 
