d4b07bfa0511e8c951fd05e02c02c24ef6f0e2cf
Adam L. Taylor <taylora@janelia.hhmi.org>
2025-10-03 14:08:53 -0400

    deploy: Fixed warning about nBowlMarkers not being a supported optional argument

--- utility/filterRegistrationParamsForCircleMethod.m
+++ utility/filterRegistrationParamsForCircleMethod.m
@@ -3,10 +3,15 @@ function registration_params_cell = filterRegistrationParamsForCircleMethod(regi
 % passing to determineRegistrationTransformForCircleMethodCore().
 % This is a pure function.
 
-fnsignore = intersect(fieldnames(registration_params),...
+fieldNamesToRemove = ...
   {'minFliesLoadedTime','maxFliesLoadedTime','extraBufferFliesLoadedTime','usemediandt','doTemporalRegistration','OptogeneticExp', ...
    'LEDMarkerType','maxDistCornerFrac_LEDLabel','doTemporalTruncation','maxFlyTrackerNanInterpFrames', 'bkgdNSampleFrames','featureRadius',...
-   'bowlMarkerType','maxDistCornerFrac_BowlLabel','method','doesYAxisPointUp'});
-registration_params_cell = struct2paramscell(rmfield(registration_params,fnsignore));
+   'bowlMarkerType','maxDistCornerFrac_BowlLabel','method','doesYAxisPointUp','nBowlMarkers'} ;
+% rmfield() will error if you try to remove a nonexistant field, so need to
+% intersect with the existing field names.
+originalFieldNames = fieldnames(registration_params) ;
+originalFieldNamesToRemove = intersect(originalFieldNames, fieldNamesToRemove) ;
+filtered_registration_params = rmfield(registration_params,originalFieldNamesToRemove) ;
+registration_params_cell = struct2paramscell(filtered_registration_params);
 
 end
