e7c92c268fee53b027d2b2aec18dfa091d3ed6b3
arobie <arobie@gmail.com>
2026-02-02 13:15:31 -0500

    updated phase diff codes to take absolute value for contralateral stats

--- locomotion/computeContinuousPhaseDiff_linearinterp.m
+++ locomotion/computeContinuousPhaseDiff_linearinterp.m
@@ -88,66 +88,159 @@ if debug
             limbs(2), size(loctall{limbs(2)},2), size(locball{limbs(2)},2))
     end
 end
-
 % compute diffs
 legorder = {'RF','RM','RH','LH','LM','LF'};
 
-% first leg in reference
-diffs = [5, 1
-    5, 2
-    5, 3
-    5, 4
-    5, 6
-    6,1
-    5,2
-    4,3
-    6,5
-    1,2
-    2,3
-    1,5
-    6,2
-    2,4];
+% second leg is reference
+diffs = [5,4 % middle - hind (left?) LM_
+    2,3 % middle - hind (right?)
+    6,5 % front - middle (left?)
+    1,2 % front - middle (right?)
+    2,4 % contra middle - hind (tripods)
+    5,3 % contra middle - hind (tripods)
+    6,2 % contra front - middle (tripods)
+    1,5]; % contra front - middle (tripods)
 
-for d = 1:numel(diffs)/2
+
+
+% within segment contralateral pairs 
+absdiffs = [1,6
+    2,5
+    3,4];
+
+
+for d = 1:size(diffs,1)
+    % measurement - reference, postive = advance, negative = delay
     name = [legorder{diffs(d,1)},'_',legorder{diffs(d,2)}];
-   % % NOTE using diff so limbs(2) - limbs(1) so reversing diffs values to
-   % % make reference correct.
-   % % limbs = [diffs(d,2),diffs(d,1)];
-   % % currdata = wrapTo2Pi(diff(unwrap(legphases(limbs,:),[],2)));
-   % % compute circ_dist instead ... 
+    
+    % compute circ_dist
     limbs = [diffs(d,1),diffs(d,2)];
     currdata = circ_dist(legphases(limbs(1),:),legphases(limbs(2),:));
-    phasediff_interp.(name).data = currdata;
+
+    % % debug
+    % figure,
+    % hold on
+    % plot(legphases(limbs(1),:),'.k')
+    % plot(legphases(limbs(2),:),'.r')
+
+    phasediff_hilbert.(name).data = currdata;
     % reported in -pi to pi
-    phasediff_interp.(name).mean = circ_mean(currdata(~isnan(currdata)));
-    phasediff_interp.(name).std = circ_std(currdata(~isnan(currdata)));
-    phasediff_interp.(name).n = numel(currdata(~isnan(currdata)));
+    phasediff_hilbert.(name).mean = circ_mean(currdata(~isnan(currdata)));
+    phasediff_hilbert.(name).std = circ_std(currdata(~isnan(currdata)));
+    phasediff_hilbert.(name).n = numel(~isnan(currdata));
+
 end
+
+% for contralateral legs of each segment, take absolute value of
+% differences -> range 0,pi. Then use regular mean, std. 
+for d = 1:size(absdiffs,1)
+    name = ['abs',legorder{absdiffs(d,1)},'_',legorder{absdiffs(d,2)}];
+    limbs = [absdiffs(d,1),absdiffs(d,2)];
+    currdata = abs(circ_dist(legphases(limbs(1),:),legphases(limbs(2),:)));
+    phasediff_hilbert.(name).data = currdata;
+    phasediff_hilbert.(name).mean = mean(currdata(~isnan(currdata)));
+    phasediff_hilbert.(name).std = std(currdata(~isnan(currdata)));
+    phasediff_hilbert.(name).n = nnz(~isnan(currdata));
+
+end
+
 
 % group ipsi and contra
 phasegroups = struct;
 phasegroups.ipsi_post_2 = {'LM_LH','RM_RH'};
 phasegroups.ipsi_ant_2 = {'LF_LM','RF_RM'};
-phasegroups.ipsi_A2P_4 = {'LM_LH','RM_RH','LF_LM','RF_RM'};
-phasegroups.conta_L2R_3 = {'LF_RF','LM_RM','LH_RH'};
+phasegroups.ipsi_P2A_4 = {'LM_LH','RM_RH','LF_LM','RF_RM'};
 phasegroups.tripods_4 = {'RF_LM','LM_RH','LF_RM','RM_LH'};
+
 
 % combine phase groups
 flds = fields(phasegroups);
 for f= 1:numel(flds)
-phasegroupname = flds{f};
-phasegroup = phasegroups.(phasegroupname);
+    phasegroupname = flds{f};
+    phasegroup = phasegroups.(phasegroupname);
 
+    curr_all = {};
+    for p = 1:numel(phasegroup)
+        curr_all{p} = phasediff_hilbert.(phasegroup{p}).data;
+    end
+    diffdata = horzcat(curr_all{:});
+    phasediff_hilbert.(phasegroupname).data = diffdata;
+    phasediff_hilbert.(phasegroupname).mean = circ_mean(diffdata(~isnan(diffdata)));
+    phasediff_hilbert.(phasegroupname).std = circ_std(diffdata(~isnan(diffdata)));
+    phasediff_hilbert.(phasegroupname).n = nnz(~isnan(diffdata));
+end
+
+% absolute contralateral: use non-circular stats 
+phasegroupname = 'abscontra_L2R_3';
+phasegroup = {'absRF_LF','absRM_LM','absRH_LH'};
 curr_all = {};
 for p = 1:numel(phasegroup)
-    curr_all{p} = phasediff_interp.(phasegroup{p}).data;
+    curr_all{p} = phasediff_hilbert.(phasegroup{p}).data;
 end
-diffdata = [];
 diffdata = horzcat(curr_all{:});
-phasediff_interp.(phasegroupname).data = diffdata;
-phasediff_interp.(phasegroupname).mean = circ_mean(diffdata(~isnan(diffdata)));
-phasediff_interp.(phasegroupname).std = circ_std(diffdata(~isnan(diffdata)));
-phasediff_interp.(phasegroupname).n = numel(diffdata(~isnan(diffdata)));
+phasediff_hilbert.(phasegroupname).data = diffdata;
+phasediff_hilbert.(phasegroupname).mean = mean(diffdata,'omitnan');
+phasediff_hilbert.(phasegroupname).std = std(diffdata,'omitnan');
+phasediff_hilbert.(phasegroupname).n = nnz(~isnan(diffdata));
+% % compute diffs
+% legorder = {'RF','RM','RH','LH','LM','LF'};
+% 
+% % first leg in reference
+% diffs = [5, 1
+%     5, 2
+%     5, 3
+%     5, 4
+%     5, 6
+%     6,1
+%     5,2
+%     4,3
+%     6,5
+%     1,2
+%     2,3
+%     1,5
+%     6,2
+%     2,4];
+% 
+% for d = 1:numel(diffs)/2
+%     name = [legorder{diffs(d,1)},'_',legorder{diffs(d,2)}];
+%    % % NOTE using diff so limbs(2) - limbs(1) so reversing diffs values to
+%    % % make reference correct.
+%    % % limbs = [diffs(d,2),diffs(d,1)];
+%    % % currdata = wrapTo2Pi(diff(unwrap(legphases(limbs,:),[],2)));
+%    % % compute circ_dist instead ... 
+%     limbs = [diffs(d,1),diffs(d,2)];
+%     currdata = circ_dist(legphases(limbs(1),:),legphases(limbs(2),:));
+%     phasediff_interp.(name).data = currdata;
+%     % reported in -pi to pi
+%     phasediff_interp.(name).mean = circ_mean(currdata(~isnan(currdata)));
+%     phasediff_interp.(name).std = circ_std(currdata(~isnan(currdata)));
+%     phasediff_interp.(name).n = numel(currdata(~isnan(currdata)));
+% end
+% 
+% % group ipsi and contra
+% phasegroups = struct;
+% phasegroups.ipsi_post_2 = {'LM_LH','RM_RH'};
+% phasegroups.ipsi_ant_2 = {'LF_LM','RF_RM'};
+% phasegroups.ipsi_A2P_4 = {'LM_LH','RM_RH','LF_LM','RF_RM'};
+% phasegroups.conta_L2R_3 = {'LF_RF','LM_RM','LH_RH'};
+% phasegroups.tripods_4 = {'RF_LM','LM_RH','LF_RM','RM_LH'};
+% 
+% % combine phase groups
+% flds = fields(phasegroups);
+% for f= 1:numel(flds)
+% phasegroupname = flds{f};
+% phasegroup = phasegroups.(phasegroupname);
+% 
+% curr_all = {};
+% for p = 1:numel(phasegroup)
+%     curr_all{p} = phasediff_interp.(phasegroup{p}).data;
+% end
+% diffdata = [];
+% diffdata = horzcat(curr_all{:});
+% phasediff_interp.(phasegroupname).data = diffdata;
+% phasediff_interp.(phasegroupname).mean = circ_mean(diffdata(~isnan(diffdata)));
+% phasediff_interp.(phasegroupname).std = circ_std(diffdata(~isnan(diffdata)));
+% phasediff_interp.(phasegroupname).n = numel(diffdata(~isnan(diffdata)));
 
 
 
