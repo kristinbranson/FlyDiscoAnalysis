b0961db031f92ee459057a7e2830130da3ea5ac8
Adam L. Taylor <taylora@janelia.hhmi.org>
2026-03-03 01:33:37 -0500

    main: Add support for fastcomputepffs boolean in JAABADetect stage params file

--- stages/FlyDiscoJAABADetect.m
+++ stages/FlyDiscoJAABADetect.m
@@ -12,20 +12,31 @@ if isempty(analysis_protocol) ,
   error('analysis_protocol cannot be empty') ;
 end
 
-% Read in the names of the .jab files
+% Read the data location parameters
 datalocparamsfilestr = 'dataloc_params.txt' ;
 datalocparamsfile = fullfile(settingsdir,analysis_protocol,datalocparamsfilestr) ;
 dataloc_params = ReadParams(datalocparamsfile) ;
+
+% Read in the names of the .jab files
 jaabaclassifierparamsfilestrs = fullfile(settingsdir,analysis_protocol,dataloc_params.jaabaclassifierparamsfilestrs) ;
 jabfiles = read_one_file_name_per_line(jaabaclassifierparamsfilestrs) ;
-  
+
+% Set the fastcomputepffs option
+if isfield(dataloc_params, 'jaabadetectparamsfilestr')
+  paramsfile = fullfile(settingsdir, analysis_protocol, dataloc_params.jaabadetectparamsfilestr) ;
+  params = ReadParams(paramsfile) ;
+  fastcomputepffs = logical(params.fastcomputepffs) ;
+else
+  fastcomputepffs = false ;  
+end
+
 % Actually call JAABADetect()
 % For reasons that are unclear to me, the otherwise-useless try/catch
 % wrapper ensures that errors encounted during loading of user-defined
 % class objects are still silently ignored, even when "dbstop if error"
 % is engaged.  ALT, 2021-03-04
 try
-  JAABADetect(expdir, 'jabfiles', jabfiles, 'forcecompute', forcecompute) ;
+  JAABADetect(expdir, 'jabfiles', jabfiles, 'forcecompute', forcecompute, 'fastcomputepffs', fastcomputepffs) ;
 catch me ,
   rethrow(me) ;
 end
